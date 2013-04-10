12c2db86854f4561731df91ec1f0264e4c726baf
Momin Khan <foolmoron@gmail.com>
2013-04-10 17:16:33 -0400

    Again

---
+++ Custom Scripts/behaviors/CQfaultind/runtime.js
@@ -0,0 +1,119 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+// *** CHANGE THE BEHAVIOR ID HERE *** - must match the "id" property in edittime.js
+//           vvvvvvvvvv
+cr.behaviors.CQfaultind = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	// *** CHANGE THE BEHAVIOR ID HERE *** - must match the "id" property in edittime.js
+	//                               vvvvvvvvvv
+	var behaviorProto = cr.behaviors.CQfaultind.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+		this.TOTAL_FREEZE_TIME = 1;
+		this.freezeTime = 0;
+		this.alreadyUnfroze = false;
+	};
+
+	behinstProto.tick = function ()
+	{
+		var dt = this.runtime.getDt(this.inst);
+		
+		this.freezeTime += dt;
+		if (!this.alreadyUnfroze && this.freezeTime >= this.TOTAL_FREEZE_TIME){
+			this.unfreeze();
+			this.alreadyUnfroze = true;
+		}
+	};	
+	
+	behinstProto.unfreeze = function ()
+	{
+		this.runtime.timescale = 1;
+		var x = CQ.getTopLayer();
+		
+		var tangentObj = this.runtime.createInstance(CQ.getType("CQFault"), this.runtime.running_layout.layers[CQ.getBottomLayer()], this.inst.x, this.inst.y);
+		tangentObj.angle = this.inst.angle;
+		this.runtime.all_global_vars[1].data += 1;		
+		
+		this.runtime.DestroyInstance(this.inst);
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+
+	// the example condition
+	Cnds.prototype.IsMoving = function ()
+	{
+		// ... see other behaviors for example implementations ...
+		return false;
+	};
+	
+	// ... other conditions here ...
+	
+	behaviorProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	
+	// ... other actions here ...
+	
+	behaviorProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+
+	// the example expression
+/* 	Exps.prototype.MyExpression = function (ret)	// 'ret' must always be the first parameter - always return the expression's result through it!
+	{
+		ret.set_int(1337);				// return our value
+		// ret.set_float(0.5);			// for returning floats
+		// ret.set_string("Hello");		// for ef_return_string
+		// ret.set_any("woo");			// for ef_return_any, accepts either a number or string
+	}; */
+	
+	// ... other expressions here ...
+	
+	behaviorProto.exps = new Exps();
+	
+}());
